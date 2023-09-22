cb60c1fb9224b5c8940af4961726f3325d2222a6
Sadir Sánchez Valderrama <sadir.sanchez@gmail.com>
2023-09-22 07:28:17 -0500

    Inicio Proyecto Archivo

---
+++ Controller/Archivo.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var botonRegresar = document.getElementById('botonRegresar');
+  
+  botonRegresar.addEventListener('click', function() {
+      window.location.href = 'Documentos.html';
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var options = {
+    format: 'dd/mm/yyyy', 
+    autoClose: true, 
+    
+  };
+  var instances = M.Datepicker.init(elems, options);
+});
+
+

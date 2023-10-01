9aec635e660f0322fb8bd2729a8bacc42ba85242
sairsanchez <sairsanchez29@gmail.com>
2023-10-01 10:54:30 -0500

    menu materialize instalado

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <div class="nav-wrapper blue lighten-1">
+      <a class="NavInicio" href="pantallaprincipal.html">Inicio</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="Documentos.html">Documentos</a></li>
+        <li><a href="Usuarios.html">Usuarios</a></li>
+        <li><a href="Alertasyestadistica.html">Alertas y estadisticas</a></li>
+        <li><a href="Manual.html">Manual</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>

d00c99bb9b9e6005fa5214c41e12b6d2bca0c485
Sadir Sánchez Valderrama <sadir.sanchez@gmail.com>
2023-10-03 16:59:09 -0500

    Se elabora formulario del menú Document/Consult completo

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <input type="date" v-model="selectedDate">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DatePicker',
+    data() {
+      return {
+        selectedDate: '',
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos CSS específicos del componente si es necesario */
+  </style>
+  
--- src/views/Documents/Consult.vue
+++ src/views/Documents/Consult.vue
@@ -1,9 +1,68 @@
 <template>
-    <h1>Prueba Consultar</h1>
+    <h1 class="text-success text-center mt-3">Consultar Documentos</h1>
+
+    <div class="container-fluid">
+        <div class="row mt-3">
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="documentType" class="form-label">Tipo de documento</label>
+                    <select id="documentType" class="form-select">
+                        <option selected>Seleccionar</option>
+                        <option value="actas">Actas</option>
+                        <option value="resoluciones">Resoluciones</option>
+                        <option value="decretos">Decretos</option>
+                        <option value="contratos">Contratos</option>
+                        <option value="historia-laboral">Historia Laboral</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="documentName" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" id="documentName">
+                </div>
+                <div class="mb-3">
+                    <label for="documentCode" class="form-label">Código</label>
+                    <input type="text" class="form-control" id="documentCode">
+                </div>
+
+                <div class="mb-3">
+                    <label for="productionUnit" class="form-label">Dependencia Productora</label>
+                    <select id="productionUnit" class="form-select">
+                        <option selected>Seleccionar</option>
+                        <option value="despacho">Despacho</option>
+                        <option value="gobierno">Gobierno</option>
+                        <option value="secGeneral">Secretaria General</option>
+                        <option value="hacienda">Hacienda</option>
+                        <option value="planeacion">Planeación</option>
+                        <option value="educacion">Educación</option>
+                        <option value="salud">Salud</option>
+                        <option value="transito">Transito</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label class="form-label">Fecha de elaboración</label>
+                    <date-picker class="my-3"></date-picker>
+                </div>
+
+                <button type="submit" class="btn btn-success">Consultar</button>
+                <button type="submit" class="btn btn-success mx-3">Regresar</button>
+
+                <ul class="list-group mt-4">
+                    <li class="list-group-item">Documento 1</li>
+                    <li class="list-group-item">Documento 2</li>
+                    <li class="list-group-item">Documento 3</li>
+                    <li class="list-group-item">Documento 4</li>
+                    <li class="list-group-item">Documento ...</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
 </template>
      
    
 <script>
+
 
 
 export default {
@@ -15,6 +74,7 @@ export default {
 
     data() {
         return {
+
 
         };
     },
